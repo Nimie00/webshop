0434cdf3dc578288ebe335e6c16575d7532ed1d7
nimie <h267734@stud.u-szeged.hu>
2024-05-16 18:22:50 +0200

    starter

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnsvNiTGhxDpk1fPlSnmIQzqzmaWynf-Q",
+  authDomain: "webshop-nimie.firebaseapp.com",
+  projectId: "webshop-nimie",
+  storageBucket: "webshop-nimie.appspot.com",
+  messagingSenderId: "611342204329",
+  appId: "1:611342204329:web:ae62d52b25800ae92398b8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
